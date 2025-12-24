2a79b712805e7f039fe6f8536e5fb2b6d7f000b5
LemanGurbanova <lemangrbanova222@gmail.com>
2025-12-24 23:41:25 +0400

    category  and product added

---
+++ Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pronia.Contexts;
+using Pronia.Models;
+
+namespace Pronia.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        private void SendCategoriesWithViewBag()
+        {
+            ViewBag.Categories = _context.Categories.ToList();
+        }
+
+        public IActionResult Index()
+        {
+            var products = _context.Products
+                .Include(p => p.Category)
+                .ToList();
+
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            SendCategoriesWithViewBag();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                SendCategoriesWithViewBag();
+                return View(product);
+            }
+
+            bool existsCategory = _context.Categories
+                .Any(c => c.Id == product.CategoryId);
+
+            if (!existsCategory)
+            {
+                ModelState.AddModelError("CategoryId", "Category is not valid");
+                SendCategoriesWithViewBag();
+                return View(product);
+            }
+
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+                return NotFound();
+
+            SendCategoriesWithViewBag();
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                SendCategoriesWithViewBag();
+                return View(product);
+            }
+
+            bool existsCategory = _context.Categories
+                .Any(c => c.Id == product.CategoryId);
+
+            if (!existsCategory)
+            {
+                ModelState.AddModelError("CategoryId", "Category is not valid");
+                SendCategoriesWithViewBag();
+                return View(product);
+            }
+
+            var dbProduct = _context.Products.Find(product.Id);
+            if (dbProduct == null)
+                return NotFound();
+
+            dbProduct.Name = product.Name;
+            dbProduct.Description = product.Description;
+            dbProduct.Price = product.Price;
+            dbProduct.SKU = product.SKU;
+            dbProduct.CategoryId = product.CategoryId;
+            dbProduct.MainImageUrl = product.MainImageUrl;
+            dbProduct.HoverImageUrl = product.HoverImageUrl;
+
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+                return NotFound();
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+
+
+
+    }
+}
--- obj/Debug/net8.0/Pronia.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pronia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ac93a2e3b6fb5042a60e65d4df45008193853b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1595695c849e6ccd004a2af8d024b2a714a6ab9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
